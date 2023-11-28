0554f54ea03ba03a3bf771092f86a34b202d4898
devVibe11 <151621298+devVibe11@users.noreply.github.com>
2023-11-28 22:57:31 +0530

    Update story.js
    some mistake in code

--- instastory/story.js
+++ instastory/story.js
@@ -23,17 +23,20 @@ let arr = [
             "https://images.unsplash.com/photo-1701009203360-ba64a6d53f5c?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8eEh4WVRNSExnT2N8fGVufDB8fHx8fA%3D%3D",
     }
 ]
-let clutter = ""
+let clutter = "";
 arr.forEach(function (elem, idx) {
-    clutter += `<div class="story"><img id="${idx}" src="${elem.dp}" alt=""></div>`
+    clutter += `<div class="story" id="${idx}"><img src="${elem.dp}" alt=""></div>`;
+});
 
-})
-document.querySelector("#card").innerHTML = clutter
-document.querySelector("#card").addEventListener("click", function (dets) {
-    document.querySelector("#full-screen").style.display = "block"
-    document.querySelector("#full-screen").style.backgroundImage = `url(${arr[dets.target.id].story})`
+document.querySelector("#card").innerHTML = clutter;
 
-    setTimeout(function(){
-        document.querySelector("#full-screen").style.display = "none"
-    },3000)
+document.querySelectorAll(".story").forEach(function (storyElement) {
+    storyElement.addEventListener("click", function (event) {
+        document.querySelector("#full-screen").style.display = "block";
+        document.querySelector("#full-screen").style.backgroundImage = `url(${arr[event.currentTarget.id].story})`;
+
+        setTimeout(function () {
+            document.querySelector("#full-screen").style.display = "none";
+        }, 3000);
+    });
 });
